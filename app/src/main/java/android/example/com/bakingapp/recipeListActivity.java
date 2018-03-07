c4e1cf0729b62edc5554a0da1b3aa72f4105b03a
Florin Bacu <bacuflorin@yahoo.com>
2018-03-07 16:46:20 +0200

    Fix a tiny error

--- app/src/main/java/android/example/com/bakingapp/recipeListActivity.java
+++ app/src/main/java/android/example/com/bakingapp/recipeListActivity.java
@@ -73,7 +73,7 @@ public class recipeListActivity extends AppCompatActivity {
     public static class SimpleItemRecyclerViewAdapter
             extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
 
-        private final ItemListActivity mParentActivity;
+        private final recipeListActivity mParentActivity;
         private final List<DummyContent.DummyItem> mValues;
         private final boolean mTwoPane;
         private final View.OnClickListener mOnClickListener = new View.OnClickListener() {

fa67eab78bbcde1f46547f855e275b6d563e78a2
Florin Bacu <bacuflorin@yahoo.com>
2018-03-07 19:02:56 +0200

    Fix DataLoader

--- app/src/main/java/android/example/com/bakingapp/Concepts/DataLoader.java
+++ app/src/main/java/android/example/com/bakingapp/Concepts/DataLoader.java
@@ -5,6 +5,7 @@ import android.example.com.bakingapp.R;
 import android.example.com.bakingapp.recipeListActivity;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Toast;
@@ -50,35 +51,28 @@ public class DataLoader {
 
     public static void load(final Context context) throws InterruptedException {
         if (!DataIsLoaded) {
-            Thread thread=new Thread(new Runnable() {
-                private boolean verifyInternetConnection() {
+            try {
+                new AsyncTask<URL, Integer, String>() {
+                    boolean isConnected;
 
-                    ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-
-                    NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
-                    boolean isConnected = activeNetwork != null &&
-                            activeNetwork.isConnectedOrConnecting();
-                    return isConnected;
-                }
-                private ArrayList<Ingredient> makeIngredientsList(JSONArray ingredientsArray) throws JSONException {
-                    int i;
-                    ArrayList<Ingredient> ingredients=new ArrayList<Ingredient>();
-                    for(i=0;i<ingredientsArray.length();i++)
-                    {
+                    private ArrayList<Ingredient> makeIngredientsList(JSONArray ingredientsArray) throws JSONException {
+                        int i;
+                        ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>();
+                        for (i = 0; i < ingredientsArray.length(); i++) {
 
                             ingredients.add(new Ingredient(ingredientsArray.getJSONObject(i).getInt("quantity"),
                                     ingredientsArray.getJSONObject(i).getString("ingredient"),
                                     ingredientsArray.getJSONObject(i).getString("measure")
-                                    ));
+                            ));
 
+                        }
+                        return ingredients;
                     }
-                    return ingredients;
-                }
-                private ArrayList<Step> makeStepList(JSONArray stepsArray) throws JSONException {
-                    ArrayList<Step> steps=new ArrayList<Step>();
-                    int i;
-                    for(i=0;i<stepsArray.length();i++)
-                    {
+
+                    private ArrayList<Step> makeStepList(JSONArray stepsArray) throws JSONException {
+                        ArrayList<Step> steps = new ArrayList<Step>();
+                        int i;
+                        for (i = 0; i < stepsArray.length(); i++) {
 
                             steps.add(new Step(stepsArray.getJSONObject(i).getInt("id"),
                                     stepsArray.getJSONObject(i).getString("shortDescription"),
@@ -86,85 +80,96 @@ public class DataLoader {
                                     stepsArray.getJSONObject(i).getString("videoURL"),
                                     stepsArray.getJSONObject(i).getString("thumbnailURL")));
 
-                    }
-                    return steps;
-                }
-                private void proccessJSON(String json) throws JSONException {
-                    JSONArray recipesArray=new JSONArray(json);
-                    JSONObject recipeObject;
-                    DataLoader.COUNT=recipesArray.length();
-                    int i;
-                    int id;
-                    String name;
-                    ArrayList<Ingredient> ingredients;
-                    ArrayList<Step> steps;
-                    int servings;
-                    String image;
-                    for(i=0;i<DataLoader.COUNT;i++)
-                    {
-                        recipeObject=recipesArray.getJSONObject(i);
-                        id=recipeObject.getInt("id");
-                        name=recipeObject.getString("name");
-                        ingredients=makeIngredientsList(recipeObject.getJSONArray("ingredients"));
-                        steps=makeStepList(recipeObject.getJSONArray("steps"));
-                        servings=recipeObject.getInt("servings");
-                        image=recipeObject.getString("image");
-                        DataLoader.addRecipe(new Recipe(id,name,ingredients,steps,servings,image));
-
-                    }
-
-
-
-
-                }
-
-                @Override
-                public void run() {
-                    if(!verifyInternetConnection())
-                    {
-                        Toast.makeText(context,"No internet connection",Toast.LENGTH_LONG).show();
-                        return;
-                    }
-                    String source = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
-                    URL url = null;
-                    try {
-                        url = new URL(source);
-                    } catch (MalformedURLException e) {
-                        e.printStackTrace();
-                    }
-                    String json = null;
-                    HttpURLConnection urlConnection = null;
-                    try {
-
-                        urlConnection = (HttpURLConnection) url.openConnection();
-                        InputStream in = urlConnection.getInputStream();
-                        Scanner scanner = new Scanner(in);
-                        scanner.useDelimiter("\\A");
-                        boolean hasInput = scanner.hasNext();
-
-                        if (hasInput) {
-                            json = scanner.next();
-                        } else {
-                            json = null;
                         }
-
-                    } catch (IOException e) {
-                        urlConnection.disconnect();
-                    }
-                    DataIsLoaded = (json != null);
-                    try {
-                        proccessJSON(json);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
+                        return steps;
                     }
 
-                }
+                    @Override
+                    protected void onPostExecute(String json) {
+                        if (json == null)
+                            return;
+                        JSONArray recipesArray = null;
+                        try {
+                            recipesArray = new JSONArray(json);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        JSONObject recipeObject;
+                        DataLoader.COUNT = recipesArray.length();
+                        int i;
+                        int id;
+                        String name;
+                        ArrayList<Ingredient> ingredients;
+                        ArrayList<Step> steps;
+                        int servings;
+                        String image;
+                        for (i = 0; i < DataLoader.COUNT; i++) {
+                            try {
+                                recipeObject = recipesArray.getJSONObject(i);
+
+                                id = recipeObject.getInt("id");
+
+                                name = recipeObject.getString("name");
+                                ingredients = makeIngredientsList(recipeObject.getJSONArray("ingredients"));
+                                steps = makeStepList(recipeObject.getJSONArray("steps"));
+                                servings = recipeObject.getInt("servings");
+                                image = recipeObject.getString("image");
+                                DataLoader.addRecipe(new Recipe(id, name, ingredients, steps, servings, image));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
 
 
-            });
+                        }
+                        setupRecyclerView();
+                    }
 
-            thread.start();
 
+                    @Override
+                    protected void onPreExecute() {
+                        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+                        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+                        isConnected = activeNetwork != null &&
+                                activeNetwork.isConnectedOrConnecting();
+
+                    }
+
+                    @Override
+                    protected String doInBackground(URL... urls) {
+                        if (!isConnected) {
+                            return null;
+                        }
+                        URL url = urls[0];
+
+                        String json = null;
+                        HttpURLConnection urlConnection = null;
+                        try {
+
+                            urlConnection = (HttpURLConnection) url.openConnection();
+                            InputStream in = urlConnection.getInputStream();
+                            Scanner scanner = new Scanner(in);
+                            scanner.useDelimiter("\\A");
+                            boolean hasInput = scanner.hasNext();
+
+                            if (hasInput) {
+                                json = scanner.next();
+                            } else {
+                                json = null;
+                            }
+
+                        } catch (IOException e) {
+                            urlConnection.disconnect();
+                        }
+                        DataIsLoaded = (json != null);
+                        return json;
+                    }
+                }.execute(new URL("https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json"));
+            }
+            catch (MalformedURLException mue)
+            {
+
+            }
         }
         else
         {
